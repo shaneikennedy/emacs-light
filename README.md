6335d5de09ae685d417edded03813a50e09bccf2
Shane Kennedy <shane.kennedy19@gmail.com>
2021-10-09 14:40:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Emacs (light) - my lightweight emacs config with only the necessities.
+Ok and a few nice things, too.
+
+I wanted to keep this as simple as possible to it is a 1-file config that is split by comments. 95% of the config I would consider essential and the other 5% is by taste. 
+
+For the nice things I've put everything below the `;; Being improvements` comment. This consists of a nice window management package called "ace-window", a package called "smex" for sorting the M-x menu, and "evil-mode" because I am a sinner.
+
+## Installing
+``` shell
+git clone https://github.com/shaneikennedy/emacs-light ~/emacs-light
+ln -s ~/emacs-light/init.el ~/.emacs
+```
+
+To see my full config with all the bells and whistles check it out [here](https://github.com/shaneikennedy/.emacs.d)
---
+++ init.el
@@ -0,0 +1,173 @@
+(require 'package)
+
+(defmacro append-to-list (target suffix)
+  "Append SUFFIX to TARGET in place."
+  `(setq ,target (append ,target ,suffix)))
+
+(append-to-list package-archives
+                '(("melpa" . "http://melpa.org/packages/") ;; Main package archive
+                  ("melpa-stable" . "http://stable.melpa.org/packages/") ;; Some packages might only do stable releases?
+                  ("org-elpa" . "https://orgmode.org/elpa/"))) ;; Org packages, I don't use org but seems like a harmless default
+
+(package-initialize)
+
+;; Ensure use-package is present. From here on out, all packages are loaded
+;; with use-package. Also, refresh the package archive on load so we can pull the latest packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package exec-path-from-shell
+  :config
+  (exec-path-from-shell-initialize))
+
+;; Allow navigation between use-package stanzas with imenu.
+;; This has to be set before loading use-package.
+(defvar use-package-enable-imenu-support t)
+(require 'use-package)
+(setq
+ use-package-always-ensure t ;; Makes sure to download new packages if they aren't already downloaded
+ use-package-verbose t) ;; Package install logging. Packages break, nice to know why.
+
+;; Any Customize-based settings should live in custom.el, not here.
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+(use-package doom-themes
+  :init
+  (load-theme 'doom-one))
+
+;;; OS specific config
+(defconst *is-a-mac* (eq system-type 'darwin))
+(defconst *is-a-linux* (eq system-type 'gnu/linux))
+
+;; Emacs feels like it's developed with linux in mind, here are some mac UX improvments
+(when *is-a-mac*
+  (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'none)
+  (setq default-input-method "MacOSX"))
+
+;; Some linux love, too
+(when *is-a-linux*
+  (setq x-super-keysym 'meta))
+
+;; Fullscreen by default, as early as possible. This tiny window is not enough
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Make M-x and other mini-buffers sortable, filterable
+(use-package ivy
+  :init
+  (ivy-mode 1)
+  (unbind-key "S-SPC" ivy-minibuffer-map)
+  (setq ivy-height 15
+        ivy-use-virtual-buffers t
+        ivy-use-selectable-prompt t))
+
+(use-package counsel
+  :after ivy
+  :init
+  (counsel-mode 1)
+  :bind (:map ivy-minibuffer-map))
+
+;; Company is the best Emacs completion system.
+(use-package company
+  :bind (("C-." . company-complete))
+  :custom
+  (company-idle-delay 0) ;; I always want completion, give it to me asap
+  (company-dabbrev-downcase nil "Don't downcase returned candidates.")
+  (company-show-numbers t "Numbers are helpful.")
+  (company-tooltip-limit 10 "The more the merrier.")
+  :config
+  (global-company-mode) ;; We want completion everywhere
+
+  ;; use numbers 0-9 to select company completion candidates
+  (let ((map company-active-map))
+    (mapc (lambda (x) (define-key map (format "%d" x)
+                        `(lambda () (interactive) (company-complete-number ,x))))
+          (number-sequence 0 9))))
+
+;; Flycheck is the newer version of flymake and is needed to make lsp-mode not freak out.
+(use-package flycheck
+  :config
+  (add-hook 'prog-mode-hook 'flycheck-mode) ;; always lint my code
+  (add-hook 'after-init-hook #'global-flycheck-mode))
+
+;; Package for interacting with language servers
+(use-package lsp-mode
+  :commands lsp
+  :config
+  (setq lsp-prefer-flymake nil ;; Flymake is outdated
+        lsp-headerline-breadcrumb-mode nil)) ;; I don't like the symbols on the header a-la-vscode, remove this if you like them.
+
+;; Basic python configuration
+(use-package python
+  :init
+  (require 'python)
+  :config
+  ;; As soon as we detect python go look for that virtual env.
+  (add-hook 'python-mode-hook #'auto-virtualenv-set-virtualenv))
+
+;; Pyenv is the only reasonable way to manage python installs. What a mess.
+(use-package pyvenv)
+
+;; Auto detect my virtual env, please
+(use-package auto-virtualenv)
+
+;; Use the microsoft-python language server, it's the best available as of writing this
+(use-package lsp-python-ms
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-python-ms)
+                          (lsp))))  ; or lsp-deferred
+
+;; We need something to manage the various projects we work on
+;; and for common functionality like project-wide searching, fuzzy file finding etc.
+(use-package projectile
+  :init
+  (projectile-mode t) ;; Enable this immediately
+  :config
+  (setq projectile-enable-caching t ;; Much better performance on large projects
+        projectile-completion-system 'ivy)) ;; Ideally the minibuffer should aways look similar
+
+;; Counsel and projectile should work together.
+(use-package counsel-projectile
+  :init
+  (counsel-projectile-mode))
+
+;; This is the best git client ever.
+;; I know some people who only use emacs for magit (i.e dev in IntelliJ and commit/rebase/diff in emacs)
+;; it's that good.
+(use-package magit
+  :config
+  (magit-auto-revert-mode t) ;; Buffers will reload automatically if for example you hard-reset/revert changes in magit
+  (setq magit-completing-read-function 'ivy-completing-read)) ;; Everything should work with Ivy
+
+
+;; Begin improvements
+
+;; The best package for managing opened files that I've ever used.
+;; No more tabbing through 10 open files, and no more counting your open files to Cmd-num to.
+(use-package ace-window
+  :bind
+  ("M-o" . ace-window)
+  :config
+  (set-face-attribute 'aw-leading-char-face nil
+                      :foreground "deep sky blue"
+                      :weight 'bold
+                      :height 2.0) ;; Some nice formatting to make the letters more visible
+  (setq aw-scope 'frame)
+  (setq aw-dispatch-always t)
+  (setq aw-keys '(?q ?w ?e ?r ?a ?s ?d ?f))
+  (setq aw-dispatch-alist '((?c aw-swap-window "Ace - Swap Window")
+                            (?n aw-flip-window)))
+  (ace-window-display-mode t))
+
+;; Sort M-x by recently used, I can't believe this isn't a default already.
+(use-package smex)
+
+;; Begin evil config
+
+;; Optional for vim users
+(use-package evil
+  :init
+  (evil-mode t))
+
